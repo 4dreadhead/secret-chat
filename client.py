4d60feac4be7a71c1e4514bfbdeeea4dd8b1834d
4dreadhead <slava14042000@mail.ru>
2022-12-15 03:37:29 +0700

    Added client ui

--- client.py
+++ client.py
@@ -1,28 +1,10 @@
-import asyncio
-import sys
-import threading
-
-from PyQt6.QtWidgets import QApplication
-from qasync import QEventLoop
-from threading import Thread
-from autobahn.asyncio.websocket import WebSocketClientFactory
-from app.client.connection import ChatClient
+from PyQt6 import QtWidgets
+from app.client.chat import Chat
 
 
-def run_client():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-
-    factory = WebSocketClientFactory("ws://127.0.0.1:8080")
-    factory.protocol = ChatClient
-
-    coro = loop.create_connection(factory, '127.0.0.1', 8080)
-    loop.run_until_complete(coro)
-    loop.run_forever()
-    loop.close()
-
-
-if __name__ == '__main__':
-    t = threading.Thread(target=run_client)
-    t.start()
-    t.join()
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    ui = Chat()
+    ui.show()
+    sys.exit(app.exec())
